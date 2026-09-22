78e49e886bb78dc2726914b2a4318066129dfcae
UmranRahman <umranrahman0@gmail.com>
2026-09-22 15:19:57 -0600

    1 done

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,1 +1,43 @@
-int main() { return 0; }
+#include <iostream>
+#include <list>
+#include <random>
+#include "timer.h"
+
+constexpr size_t ARRAY_2D = 4000;
+
+int main() { 
+    //Row-major-order
+    static uint64_t rm_array[ARRAY_2D][ARRAY_2D];
+    static uint64_t cm_array[ARRAY_2D][ARRAY_2D];
+    uint64_t rm_sum = 0;
+    uint64_t cm_sum = 0;
+    std::mt19937_64 rng(0);
+
+    for (size_t i = 0; i < ARRAY_2D; i++){
+        for (size_t j = 0; j < ARRAY_2D; j++) {
+            rm_array[i][j] = rng();
+            cm_array[j][i] = rm_array[i][j];
+        }
+    }
+
+    Timer t;
+    //Row-major-order
+    for (size_t i = 0; i < ARRAY_2D; i++){
+        for (size_t j = 0; j < ARRAY_2D; j++) {
+            rm_sum += rm_array[i][j];
+        }
+    }
+    uint64_t elapsed = t.click<Timer::Micros>();
+    std::cout << elapsed << " "  << rm_sum <<"\n";
+
+    //Column-major-order
+    for (size_t j = 0; j < ARRAY_2D; j++){
+        for (size_t i = 0; i < ARRAY_2D; i++) {
+            cm_sum += cm_array[i][j];
+        }
+    }
+    uint64_t elapsed2 = t.click<Timer::Micros>();
+    std::cout << elapsed2 << " " << cm_sum <<"\n";
+
+    return 0;
+}
--- src/timer.h
+++ src/timer.h
@@ -3,4 +3,43 @@
 
 // replace with yours from the prep...
 
+#include <chrono>
+#include <cstdint>
+
+class Timer {
+public:
+    using Nanos =  std::chrono::nanoseconds;
+    using Micros = std::chrono::microseconds;
+    using Millis = std::chrono::milliseconds;
+    using Seconds = std::chrono::seconds;
+    using Minutes = std::chrono::minutes;
+    using Hours = std::chrono::hours;
+
+    Timer(){ //Default constructor
+        restart();
+    }
+    void restart(){ //resets the state of the Timer as if it has only started timing now
+        lastTime = std::chrono::steady_clock::now();
+    }
+
+    //both click and glance check for difference in time but only click updates the last time the timer was clicked
+    template <typename T>
+    uint64_t click(){
+        auto now = std::chrono::steady_clock::now();
+        auto diff = std::chrono::duration_cast<T>(now - lastTime).count();
+        lastTime = now;
+        return diff;
+    }
+
+    template <typename T> uint64_t glance(){
+        auto now = std::chrono::steady_clock::now();
+        auto diff = std::chrono::duration_cast<T>(now - lastTime).count();
+        return diff;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::steady_clock> lastTime;
+};
+
+
 #endif  // TIMER_H
